bca7b0833d31c93232cc1a06b6008c2f3d41c1eb
TanguyBarthelemy <tanguy.barthelemy@insee.fr>
2024-06-14 18:43:15 +0200

    lint package

--- tests/testthat/test-generate_chunk_header.R
+++ tests/testthat/test-generate_chunk_header.R
@@ -18,21 +18,22 @@ testthat::test_that("The header is well generated", {
     testthat::expect_identical(
         object = generate_chunk_header(error = TRUE, results = "asis",
                                        comment = "##", background = "#E7E7E7"),
-        expected = "```{r, error = TRUE, results = \"asis\", comment = \"##\", background = \"#E7E7E7\"}"
+        expected = paste0("```{r, error = TRUE, results = \"asis\", ",
+                          "comment = \"##\", background = \"#E7E7E7\"}")
     )
     testthat::expect_identical(
         object = generate_chunk_header(fig.width = 8.2, fig.height = 10,
                                        dpi = 124, warning = FALSE),
-        expected = "```{r, fig.width = 8.2, fig.height = 10, dpi = 124, warning = FALSE}"
+        expected = paste0("```{r, fig.width = 8.2, fig.height = 10, ",
+                          "dpi = 124, warning = FALSE}")
     )
 })
 
 ## Tests qui échouent
 
 testthat::test_that("The header is well generated", {
-    testthat::expect_error(object = generate_chunk_header(eval = TRUE, eval = FALSE))
+    testthat::expect_error(object = generate_chunk_header(eval = TRUE,
+                                                          eval = FALSE))
     testthat::expect_error(object = generate_chunk_header(Eval = FALSE))
     testthat::expect_error(object = generate_chunk_header(result = "markup"))
 })
-
-
--- tests/testthat/test-generate_rmd_file.R
+++ tests/testthat/test-generate_rmd_file.R
@@ -3,18 +3,35 @@ testthat::test_that("Everything works with default parameters", {
     testthat::expect_identical(
         object = generate_rmd_file(output = "word",
                                    content = "plot(AirPassengers)"),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  word_document:\n    highlight: arrow\nmonofont: \"Fira Code\"\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n## Running Code\n\n```{r}\nplot(AirPassengers)\n```\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n  ",
+                          "word_document:\n    highlight: arrow\n",
+                          "monofont: \"Fira Code\"\ncode-block-bg: ",
+                          "true\ncode-block-border-left: \"#31BAE9\"\n",
+                          "---\n\n## Running Code\n\n```{r}\n",
+                          "plot(AirPassengers)\n```\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "html",
                                    content = "plot(AirPassengers)"),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  html_document:\n    highlight: arrow\nmonofont: \"Fira Code\"\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n## Running Code\n\n```{r}\nplot(AirPassengers)\n```\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n  ",
+                          "html_document:\n    highlight: arrow\nmonofont",
+                          ": \"Fira Code\"\ncode-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "## Running Code\n\n```{r}\nplot(AirPassengers)",
+                          "\n```\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "pdf",
                                    content = "plot(AirPassengers)",
                                    fira_path = "User"),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  pdf_document:\n    highlight: arrow\n    latex_engine: xelatex\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n\\fontsize{12}{12}\n\\setmonofont[ExternalLocation=User/]{FiraCode-Regular.ttf}\n\n## Running Code\n\n```{r}\nplot(AirPassengers)\n```\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n  ",
+                          "pdf_document:\n    highlight: arrow\n    ",
+                          "latex_engine: xelatex\ncode-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "\\fontsize{12}{12}\n\\setmonofont[",
+                          "ExternalLocation=User/]{FiraCode-Regular.ttf}",
+                          "\n\n## Running Code\n\n```{r}\n",
+                          "plot(AirPassengers)\n```\n")
     )
 })
 
@@ -23,36 +40,74 @@ testthat::test_that("Everything works with custom parameters", {
         object = generate_rmd_file(output = "word",
                                    content = "Bonjour tout le monde",
                                    code = FALSE),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  word_document:\n    highlight: arrow\nmonofont: \"Fira Code\"\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n## Running Code\n\n\nBonjour tout le monde\n\n"
+        expected = paste0("---\n",
+                          "title: \"Format code\"\n",
+                          "output:\n",
+                          "  word_document:\n",
+                          "    highlight: arrow\n",
+                          "monofont: \"Fira Code\"\n",
+                          "code-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n",
+                          "---\n\n",
+                          "## Running Code\n\n\n",
+                          "Bonjour tout le monde\n\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "html",
                                    content = "Bonjour tout le monde",
                                    code = FALSE),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  html_document:\n    highlight: arrow\nmonofont: \"Fira Code\"\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n## Running Code\n\n\nBonjour tout le monde\n\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:",
+                          "\n  html_document:\n    highlight: arrow\n",
+                          "monofont: \"Fira Code\"\ncode-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "## Running Code\n\n\nBonjour tout le monde\n\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "pdf",
                                    content = "Bonjour tout le monde",
                                    code = FALSE, fira_path = "User"),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  pdf_document:\n    highlight: arrow\n    latex_engine: xelatex\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n\\fontsize{12}{12}\n\\setmonofont[ExternalLocation=User/]{FiraCode-Regular.ttf}\n\n## Running Code\n\n\nBonjour tout le monde\n\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n",
+                          "  pdf_document:\n    highlight: arrow\n",
+                          "    latex_engine: xelatex\n",
+                          "code-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "\\fontsize{12}{12}\n\\setmonofont[",
+                          "ExternalLocation=User/]{FiraCode-Regular.ttf}\n\n",
+                          "## Running Code\n\n\nBonjour tout le monde\n\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "word",
                                    content = "plot(AirPassengers)",
                                    eval = TRUE),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  word_document:\n    highlight: arrow\nmonofont: \"Fira Code\"\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n## Running Code\n\n```{r, eval = TRUE}\nplot(AirPassengers)\n```\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n",
+                          "  word_document:\n    highlight: arrow\n",
+                          "monofont: \"Fira Code\"\ncode-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "## Running Code\n\n```{r, eval = TRUE}\n",
+                          "plot(AirPassengers)\n```\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "html",
                                    content = "plot(AirPassengers)",
                                    eval = TRUE),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  html_document:\n    highlight: arrow\nmonofont: \"Fira Code\"\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n## Running Code\n\n```{r, eval = TRUE}\nplot(AirPassengers)\n```\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n",
+                          "  html_document:\n    highlight: arrow\n",
+                          "monofont: \"Fira Code\"\ncode-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "## Running Code\n\n```{r, eval = TRUE}\n",
+                          "plot(AirPassengers)\n```\n")
     )
     testthat::expect_identical(
         object = generate_rmd_file(output = "pdf",
                                    content = "plot(AirPassengers)",
                                    eval = TRUE, fira_path = "User"),
-        expected = "---\ntitle: \"Format code\"\noutput:\n  pdf_document:\n    highlight: arrow\n    latex_engine: xelatex\ncode-block-bg: true\ncode-block-border-left: \"#31BAE9\"\n---\n\n\\fontsize{12}{12}\n\\setmonofont[ExternalLocation=User/]{FiraCode-Regular.ttf}\n\n## Running Code\n\n```{r, eval = TRUE}\nplot(AirPassengers)\n```\n"
+        expected = paste0("---\ntitle: \"Format code\"\noutput:\n",
+                          "  pdf_document:\n    highlight: arrow\n",
+                          "    latex_engine: xelatex\ncode-block-bg: true\n",
+                          "code-block-border-left: \"#31BAE9\"\n---\n\n",
+                          "\\fontsize{12}{12}\n\\setmonofont[",
+                          "ExternalLocation=User/]{FiraCode-Regular.ttf}\n\n",
+                          "## Running Code\n\n```{r, eval = TRUE}\n",
+                          "plot(AirPassengers)\n```\n")
     )
 })

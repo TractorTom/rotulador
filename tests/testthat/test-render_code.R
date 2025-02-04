1d02e89adeaedfd90745aca40e7a0fde8ce45f57
Tanguy Barthelemy <tanguy.barthelemy@insee.fr>
2025-02-04 14:07:15 +0100

    Update test-render_code.R

--- tests/testthat/test-render_code.R
+++ tests/testthat/test-render_code.R
@@ -107,7 +107,7 @@ print(mtcars)",
             txt_output_pdf <- pdftools::pdf_data(path_pdf)
             txt_template_output_pdf <- pdftools::pdf_data(path_template_pdf)
 
-            file.copy(txt_output_pdf, "./output.pdf")
+            file.copy(path_pdf, "./output.pdf")
             
             testthat::expect_identical(object = txt_output_pdf,
                                        expected = txt_template_output_pdf)
